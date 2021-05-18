277eb1794f652f90e31be310cdffbd7957dd50f3
shaoqing <zhangshaoqing@idcm.io>
2021-05-18 18:14:30 +0800

    first commit

---
+++ Example/Pods/Target Support Files/CommonElement/CommonElement-umbrella.h
@@ -0,0 +1,39 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSArray+BinAdd.h"
+#import "NSData+BinAdd.h"
+#import "NSDate+BinAdd.h"
+#import "NSDictionary+BinAdd.h"
+#import "NSMutableArray+BinAdd.h"
+#import "NSMutableDictionary+BinAdd.h"
+#import "NSNotificationCenter+BinAdd.h"
+#import "NSDecimalNumber+BinAdd.h"
+#import "NSObject+BinAdd.h"
+#import "NSString+BinAdd.h"
+#import "UIColor+BinAdd.h"
+#import "UIControl+BinAdd.h"
+#import "UIGestureRecognizer+BinAdd.h"
+#import "UIImage+BinAdd.h"
+#import "UILabel+FTDLabelExtentions.h"
+#import "UITextField+BinAdd.h"
+#import "UITextField+FFKeyBoard.h"
+#import "UIView+FrameExtention.h"
+#import "BinWebViewProtocol.h"
+#import "WKWebView+BinAdd.h"
+#import "CommonElement.h"
+#import "CommonUtils+Custom.h"
+#import "CommonUtils.h"
+
+FOUNDATION_EXPORT double CommonElementVersionNumber;
+FOUNDATION_EXPORT const unsigned char CommonElementVersionString[];
+
